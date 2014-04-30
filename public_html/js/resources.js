e386cbd58e11c48492b62fe6446b2a3422126eca
EdgarRamirez- <edgarramirez2173@gmail.com>
2014-04-30 13:22:49 -0700

    Initial commite
    Initial

---
+++ public_html/js/resources.js
@@ -0,0 +1,34 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+         { name: "background-tiles", type: "image", src: "data/img/background-tiles.png" },
+         { name: "solid-tile",type: "image", src:"data/img/solid-tile.png"},
+         { name: "cloud-tiles",type: "image", src:"data/img/cloud-tiles.png"},
+         { name: "fly-spritesheet",type: "image", src:"data/img/fly-spritesheet.png"},
+         { name: "player1-spritesheet",type: "image", src:"data/img/player1-spritesheet.png"},
+         { name: "slime-spritesheet",type: "image", src:"data/img/slime-spritesheet.png"},
+         /* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+	{ name: "Level1", type: "tmx", src: "data/map/Level1.tmx" },	
+	{ name: "Level2", type: "tmx", src: "data/map/Level2.tmx" }
+         /* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
